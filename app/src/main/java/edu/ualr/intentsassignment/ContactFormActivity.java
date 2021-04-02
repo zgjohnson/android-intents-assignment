5ddfee812d75c0bb555d5549516050e892e4069d
zgjohnson <zgjohnson@ualr.edu>
2021-04-02 10:28:03 -0500

    build gradle error

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -5,5 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ContactFormActivity extends AppCompatActivity {
     // TODO 01. Create a new layout file to define the GUI elements of the ContactFormActivity.
     // TODO 02. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
+
+    private Binding
     // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
 }
